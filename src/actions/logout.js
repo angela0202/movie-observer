8216afadad3132a0612eda4e461d9f19cef90e56
Angela <ang.hayrapetian@gmail.com>
2018-07-24 18:43:16 +0400

    [delete] unnecessary redirect

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -1,8 +1,6 @@
-import {LOGOUT_REQUEST, LOGOUT_SUCCESS} from './actionTypes';
-import history from '../helpers/history';
+import { LOGOUT_REQUEST, LOGOUT_SUCCESS } from './actionTypes';
 
-
-export const logoutRequest = () => ({type: LOGOUT_REQUEST });
+export const logoutRequest = () => ({ type: LOGOUT_REQUEST });
 
 export const logoutSuccess = () => ({ type: LOGOUT_SUCCESS });
 
@@ -11,6 +9,5 @@ export const logout = () => dispatch => {
   setTimeout(() => {
     dispatch(logoutSuccess());
     localStorage.removeItem('user');
-    history.push('/login');
   }, 1000);
 };
