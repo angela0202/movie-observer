e79184b20dfe2ca38657ac89226bb5d333294c83
Angela <ang.hayrapetian@gmail.com>
2018-07-24 23:09:44 +0400

    [fix] keep same user on page refresh

--- src/store.js
+++ src/store.js
@@ -1,20 +1,32 @@
-import {createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
 let store;
 
+const user = localStorage.getItem('user')
+  ? JSON.parse(localStorage.getItem('user'))
+  : null;
+
 if (process.env.NODE_ENV === 'production') {
-  store = createStore(rootReducer, applyMiddleware(thunk));
+  store = createStore(rootReducer,
+    {
+      currentUser : user
+    },
+    applyMiddleware(thunk));
 } else {
   store = createStore(
     rootReducer,
+    {
+      currentUser : user
+    },
     compose(
       applyMiddleware(thunk),
       window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__()
-    ));
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
 }
 
 export default store;
