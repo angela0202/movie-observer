055206bf7b98700dee4c3e580cf61794fae35ab5
Angela <ang.hayrapetian@gmail.com>
2018-07-24 11:25:50 +0400

    [add] basic routing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <h1>Movie Website</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" render={() => <div>Home page</div>} />
+          <Route path="/login" render={() => <div>Login page</div>} />
+        </Switch>
+      </Router>
     );
   }
 }
