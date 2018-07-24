b7736af9c6e00726ea3c8ecd083f6737c2635d70
Angela <ang.hayrapetian@gmail.com>
2018-07-24 16:29:35 +0400

    [add] App container

---
+++ src/containers/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+
+import App from '../../components/App';
+import { selectIsAuthenticated } from '../../selectors';
+
+const mapStateToProps = createStructuredSelector({
+  isAuthenticated: selectIsAuthenticated
+});
+
+
+export default connect(mapStateToProps)(App);
