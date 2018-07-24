cbb00ba1ac0da7a66998f1d069f45c5fbcef4468
Angela <ang.hayrapetian@gmail.com>
2018-07-24 13:59:18 +0400

    [add] loginReducer to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import {combineReducers} from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
+import loginReducer from './loginReducer';
+
 export default combineReducers({
-  form: formReducer
+  form: formReducer,
+  loginReducer
 });
