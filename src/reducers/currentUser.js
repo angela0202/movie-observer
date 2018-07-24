f0d49b9f305f31859ca758cb119a671832810aef
Angela <ang.hayrapetian@gmail.com>
2018-07-24 18:32:20 +0400

    [fix] user logout issue

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,5 +1,7 @@
 import {
   LOGIN_SUCCESS,
+  LOGOUT_SUCCESS,
+  LOGIN_FAILURE
 } from '../actions/actionTypes';
 
 const initialState = null;
@@ -8,7 +10,9 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case LOGIN_SUCCESS:
       return action.payload;
-
+    case LOGOUT_SUCCESS:
+    case LOGIN_FAILURE:
+      return null;
     default:
       return state;
   }
