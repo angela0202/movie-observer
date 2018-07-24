38f7eb032c9ffedf9827c4d5ccff5943e1dc1d80
Angela <ang.hayrapetian@gmail.com>
2018-07-24 11:35:19 +0400

    [add] private route

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import PrivateRoute from '../PrivateRoute';
+
 class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" render={() => <div>Home page</div>} />
+          <PrivateRoute exact path="/" render={() => <div>Home page</div>}/>
           <Route path="/login" render={() => <div>Login page</div>} />
         </Switch>
       </Router>
