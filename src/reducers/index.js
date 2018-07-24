7e27569ed32dd54f8deee6f151bf3cfb6fa62583
Angela <ang.hayrapetian@gmail.com>
2018-07-24 17:33:28 +0400

    [add] logout reducer to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import {combineReducers} from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
 import loginReducer from './loginReducer';
+import logoutReducer from './logoutReducer';
 import currentUser from './currentUser';
 
 
 export default combineReducers({
   form: formReducer,
   loginReducer,
-  currentUser
+  currentUser,
+  logoutReducer
 });
