cadee8b4d712fa7b14dd49efb7ee0111939756ae
Angela <ang.hayrapetian@gmail.com>
2018-07-26 08:32:19 +0400

    [add] fetching movie genres

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -2,6 +2,9 @@ import {
   POPULAR_MOVIES_FETCH_REQUEST,
   POPULAR_MOVIES_FETCH_SUCCESS,
   POPULAR_MOVIES_FETCH_FAILURE,
+  MOVIE_GENRES_FETCH_FAILURE,
+  MOVIE_GENRES_FETCH_REQUEST,
+  MOVIE_GENRES_FETCH_SUCCESS,
 } from './actionTypes';
 
 import store from '../store';
@@ -23,6 +26,21 @@ const popularMoviesFetchFailure = err => ({
   payload: { err },
 });
 
+const movieGenresFetchRequest = () => ({
+  type: MOVIE_GENRES_FETCH_REQUEST,
+  loading: true,
+});
+
+const movieGenresFetchSuccess = payload => ({
+  type: MOVIE_GENRES_FETCH_SUCCESS,
+  payload,
+});
+
+const movieGenresFetchFailure = err => ({
+  type: MOVIE_GENRES_FETCH_FAILURE,
+  payload: { err },
+});
+
 const handleErrors = res => {
   if (!res.ok) {
     throw Error(res.statusText);
@@ -39,4 +57,15 @@ export const fetchPopularMovies = () => dispatch => {
     .then(res => res.json())
     .then(payload => dispatch(popularMoviesFetchSuccess(payload)))
     .catch(err => dispatch(popularMoviesFetchFailure(err)));
+};
+
+export const fetchMovieGenres = () => dispatch => {
+  dispatch(movieGenresFetchRequest());
+  return fetch(
+    `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`
+  )
+    .then(handleErrors)
+    .then(res => res.json())
+    .then(payload => dispatch(fetchMovieGenres(payload)))
+    .catch(err => dispatch(movieGenresFetchFailure(err)));
 };
