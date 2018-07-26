b02a034419dd7eec43a24103d1a6dcf49c4ac152
Angela <ang.hayrapetian@gmail.com>
2018-07-26 08:26:36 +0400

    [add] fetching popular movies action

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -1,15 +1,42 @@
-import { FETCH_POPULAR_MOVIES } from './actionTypes';
+import {
+  POPULAR_MOVIES_FETCH_REQUEST,
+  POPULAR_MOVIES_FETCH_SUCCESS,
+  POPULAR_MOVIES_FETCH_FAILURE,
+} from './actionTypes';
+
+import store from '../store';
+
+const apiKey = store.getState().currentUser.password;
+
+const popularMoviesRequest = () => ({
+  type: POPULAR_MOVIES_FETCH_REQUEST,
+  loading: true,
+});
+
+const popularMoviesFetchSuccess = payload => ({
+  type: POPULAR_MOVIES_FETCH_SUCCESS,
+  payload,
+});
+
+const popularMoviesFetchFailure = err => ({
+  type: POPULAR_MOVIES_FETCH_FAILURE,
+  payload: { err },
+});
+
+const handleErrors = res => {
+  if (!res.ok) {
+    throw Error(res.statusText);
+  }
+  return res;
+};
 
 export const fetchPopularMovies = () => dispatch => {
-  console.log('fetching');
-  fetch(
-    'https://api.themoviedb.org/3/movie/popular?api_key=46af50cafe8e86626ec6d59ae22ad0a9&language=en-US&page=1'
+  dispatch(popularMoviesRequest());
+  return fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
   )
+    .then(handleErrors)
     .then(res => res.json())
-    .then(payload =>
-      dispatch({
-        type: FETCH_POPULAR_MOVIES,
-        payload,
-      })
-    );
+    .then(payload => dispatch(popularMoviesFetchSuccess(payload)))
+    .catch(err => dispatch(popularMoviesFetchFailure(err)));
 };
