3b8228d53764df4cbe80baafa64313e5d96c468d
Angela <ang.hayrapetian@gmail.com>
2018-07-26 18:03:03 +0400

    [split] big main component into small components

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -16,6 +16,7 @@ import { Search } from '@material-ui/icons/';
 import { Link } from 'react-router-dom';
 
 import styles from './styles';
+import MoviesList from '../../containers/MoviesList';
 
 const Main = ({ popularMovies, genres, classes, moviesLoading }) => {
   return (
@@ -39,53 +40,11 @@ const Main = ({ popularMovies, genres, classes, moviesLoading }) => {
             ),
           }}
         />
-        {moviesLoading && (
-          <div style={{ fontSize: '40px', color: 'white' }}>Please wait...</div>
-        )}
       </Paper>
-      <div className={classes.moviesContainer}>
-        {!moviesLoading &&
-          popularMovies.results && (
-            <div className={classes.movies}>
-              {popularMovies.results.map((movie, index) => (
-                <Card className={classes.card} key={index}>
-                  <CardMedia
-                    className={classes.cover}
-                    image={`https://image.tmdb.org/t/p/w200/${
-                      movie.poster_path
-                    }`}
-                    title="Live from space album cover"
-                  />
-                  <div className={classes.details}>
-                    <CardContent className={classes.content}>
-                      <Typography variant="headline" className={classes.title}>
-                        <Link
-                          to={`movie?/id=${movie.id}`}
-                          style={{ textDecoration: 'none', color: '#000' }}
-                        >
-                          {movie.title} ({movie.release_date.slice(0, 4)})
-                        </Link>
-                      </Typography>
-                      <List className={classes.list}>
-                        {movie.genre_ids.map(id => (
-                          <Chip label={genres[id]} key={id} className={classes.genre}/>
-                        ))}
-                      </List>
-
-                      <Typography component="p" className={classes.discription}>
-                        {movie.overview.substr(0, 120)}...
-                      </Typography>
-                    </CardContent>
-                    <div className={classes.controls} />
-                  </div>
-                </Card>
-              ))}
-            </div>
-          )}
-        <div className={classes.buttonsContainer}>
-          <Button>Prev</Button>
-          <Button>Next</Button>
-        </div>
+      <MoviesList/>
+      <div className={classes.buttonsContainer}>
+        <Button>Prev</Button>
+        <Button>Next</Button>
       </div>
     </Fragment>
   );
