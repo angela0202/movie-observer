7b792da112f7f9d2ded98fa457632ca861405e59
Angela <ang.hayrapetian@gmail.com>
2018-07-27 14:58:33 +0400

    [add] reducer cases for movie details action types

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -7,7 +7,10 @@ import {
   MOVIE_GENRES_FETCH_FAILURE,
   SEARCH_FETCH_REQUEST,
   SEARCH_FETCH_FAILURE,
-  SEARCH_FETCH_SUCCESS
+  SEARCH_FETCH_SUCCESS,
+  FETCH_MOVIE_DETAILS_REQUEST,
+  FETCH_MOVIE_DETAILS_FAILURE,
+  FETCH_MOVIE_DETAILS_SUCCESS
 } from '../actions/actionTypes';
 
 const initialState = {
@@ -15,6 +18,7 @@ const initialState = {
   genres: {},
   moviesLoading: false,
   genresLoading: false,
+  movieDetails: null
 };
 
 export default (state = initialState, action) => {
@@ -63,6 +67,20 @@ export default (state = initialState, action) => {
         movies: action.payload,
       };
     case SEARCH_FETCH_FAILURE:
+      return {
+        ...state,
+        err: action.payload.err,
+      };
+    case FETCH_MOVIE_DETAILS_REQUEST:
+      return {
+        ...state,
+      };
+    case FETCH_MOVIE_DETAILS_SUCCESS:
+      return {
+        ...state,
+        movieDetails: action.payload,
+      };
+    case FETCH_MOVIE_DETAILS_FAILURE:
       return {
         ...state,
         err: action.payload.err,
