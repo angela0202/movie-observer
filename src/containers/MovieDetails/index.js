c670f5b50d2ff18464192ce4d5aaf28df95212a4
Angela <ang.hayrapetian@gmail.com>
2018-07-27 15:04:06 +0400

    [add] container component for movie details

---
+++ src/containers/MovieDetails/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import MovieDetails from '../../components/MovieDetails';
+
+class MovieDetailsContainer extends Component {
+  render() {
+    return <MovieDetails />;
+  }
+}
+
+export default MovieDetailsContainer;
