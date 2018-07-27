c89cc86ade519dfa33e1b5dd948fba1b71beb1a5
Angela <ang.hayrapetian@gmail.com>
2018-07-27 12:02:30 +0400

    [delete] unused passed props

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -3,48 +3,36 @@ import { connect } from 'react-redux';
 import { reduxForm } from 'redux-form';
 
 import Main from '../../components/Main';
-import { fetchPopularMovies, fetchMovieGenres, searchMovies } from '../../actions/movieActions';
+import { fetchPopularMovies, searchMovies } from '../../actions/movieActions';
 
 class MainContainer extends Component {
-  componentDidMount() {
-    this.props.fetchPopularMovies();
-    this.props.fetchMovieGenres();
-  }
-
   handleSearch = (query, page) => {
-    !query ? this.props.fetchPopularMovies() :
-    this.props.searchMovies(query, page);
+    !query
+      ? this.props.fetchPopularMovies()
+      : this.props.searchMovies(query, page);
   };
 
   render() {
-    return <Main
-      popularMovies={this.props.movies}
-      genres={this.props.genres}
-      genresLoading={this.props.genresLoading}
-      moviesLoading={this.props.moviesLoading}
-      handleSearch={this.handleSearch}
-    />
+    return <Main handleSearch={this.handleSearch} />;
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   movies: state.movieReducer.movies,
-  genres: state.movieReducer.genres,
-  moviesLoading: state.movieReducer.moviesLoading,
-  genresLoading: state.movieReducer.genresLoading
 });
 
 const mapDispatchToProps = dispatch => ({
   fetchPopularMovies: () => dispatch(fetchPopularMovies()),
-  fetchMovieGenres: () => dispatch(fetchMovieGenres()),
-  searchMovies: (query, page) => dispatch(searchMovies(query, page))
+  searchMovies: (query, page) => dispatch(searchMovies(query, page)),
 });
 
 const formConfiguration = {
   form: 'search',
 };
 
-
 const MainWithForm = reduxForm(formConfiguration)(MainContainer);
 
-export default connect(mapStateToProps, mapDispatchToProps)(MainWithForm);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MainWithForm);
