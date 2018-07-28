01fcc06917c98f37fcdc0d9675a29a55aa0717af
Angela <ang.hayrapetian@gmail.com>
2018-07-28 18:40:38 +0400

    [add] remove from favorites action

--- src/actions/favorites.js
+++ src/actions/favorites.js
@@ -2,7 +2,8 @@ import {
   ADD_TO_FAVORITES,
   FETCH_FAVORITES_REQUEST,
   FETCH_FAVORITES_FAILURE,
-  FETCH_FAVORITES_SUCCESS
+  FETCH_FAVORITES_SUCCESS,
+  REMOVE_FROM_FAVORITES
 } from './actionTypes';
 import store from '../store';
 
@@ -33,12 +34,21 @@ const handleErrors = res => {
 };
 
 export const addToFavorites = movieID => dispatch => {
-  const movie = {
-    id: movieID,
-  };
+  const movie = movieID.toString();
+
   dispatch({ type: ADD_TO_FAVORITES, payload: movie });
   const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
   localStorage.setItem('favorites', JSON.stringify([...favorites, movie]));
+};
+
+export const removeFromFavorites = movieID => dispatch => {
+  const movie = movieID.toString();
+
+  dispatch({ type: REMOVE_FROM_FAVORITES, payload: movie });
+  const favorites = JSON.parse(localStorage.getItem('favorites'));
+  const newFavorites = favorites.filter(id => id !== movie);
+
+  localStorage.setItem('favorites', JSON.stringify(newFavorites));
 };
 
 export const fetchFavorites = (favIds) => dispatch => {
