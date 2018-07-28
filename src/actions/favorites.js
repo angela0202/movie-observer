adb480d2a367fdf28c89ecaff48b8a95ff5ee707
Angela <ang.hayrapetian@gmail.com>
2018-07-28 07:17:30 +0400

    [add] action for fetching favorite movies

--- src/actions/favorites.js
+++ src/actions/favorites.js
@@ -1,4 +1,36 @@
-import { ADD_TO_FAVORITES } from './actionTypes';
+import {
+  ADD_TO_FAVORITES,
+  FETCH_FAVORITES_REQUEST,
+  FETCH_FAVORITES_FAILURE,
+  FETCH_FAVORITES_SUCCESS
+} from './actionTypes';
+import store from '../store';
+
+const apiKey = store.getState().currentUser.password;
+
+const fetchFavoritesRequest = () => ({
+  type: FETCH_FAVORITES_REQUEST,
+  favoritesLoading: true,
+});
+
+const fetchFavoritesSuccess = payload => ({
+  type: FETCH_FAVORITES_SUCCESS,
+  payload,
+  favoritesLoading: false,
+});
+
+const fetchFavoritesFailure = err => ({
+  type: FETCH_FAVORITES_FAILURE,
+  payload: { err },
+  favoritesLoading: false,
+});
+
+const handleErrors = res => {
+  if (!res.ok) {
+    throw Error(res.statusText);
+  }
+  return res;
+};
 
 export const addToFavorites = movieID => dispatch => {
   const movie = {
@@ -7,4 +39,15 @@ export const addToFavorites = movieID => dispatch => {
   dispatch({ type: ADD_TO_FAVORITES, payload: movie });
   const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
   localStorage.setItem('favorites', JSON.stringify([...favorites, movie]));
+};
+
+export const fetchFavorites = (favIds) => dispatch => {
+  dispatch(fetchFavoritesRequest());
+
+  return Promise.all(favIds.map(movie => fetch(
+    `https://api.themoviedb.org/3/movie/${movie.id}?api_key=${apiKey}&language=en-US`
+
+  ).then(res => res.json())))
+    .then(payload => dispatch(fetchFavoritesSuccess(payload)))
+    .catch(err => dispatch(fetchFavoritesFailure(err)))
 };
