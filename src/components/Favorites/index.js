272bfff2f8fbbcac7eb3c14b9cdc57e160ed0348
Angela <ang.hayrapetian@gmail.com>
2018-07-28 07:27:40 +0400

    [add] favorites presentational component

---
+++ src/components/Favorites/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+import {List,
+  ListItem,
+  ListItemAvatar,
+  ListItemSecondaryAction,
+  ListItemText,
+  Avatar,
+  IconButton,
+  Grid,
+  Typography
+} from '@material-ui/core/';
+import FolderIcon from '@material-ui/icons/Folder';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+import styles from 'styles';
+
+const Favorites = () => {
+  return (
+    <Grid item xs={12} md={6}>
+      <Typography variant="title">Avatar with text and icon</Typography>
+      <div>
+        <List>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <FolderIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Single-line item" />
+            <ListItemSecondaryAction>
+              <IconButton aria-label="Delete">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>,
+        </List>
+      </div>
+    </Grid>
+  );
+};
+
+export default withStyles(styles)(Favorites);
