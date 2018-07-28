ae85a25fcffe53b515c65090d2d4656f4419e748
Angela <ang.hayrapetian@gmail.com>
2018-07-28 21:40:25 +0400

    [change] main background

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,6 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Field } from 'redux-form';
-import { CardMedia, Paper, TextField } from '@material-ui/core/';
+import { Paper, TextField } from '@material-ui/core/';
 import { withStyles } from '@material-ui/core/styles';
 
 import styles from './styles';
@@ -15,13 +15,14 @@ const renderTextField = ({
 
 const Main = ({ popularMovies, genres, classes, handleSearch }) => {
   return (
-    <Fragment>
-      <header className={classes.header}>
-        <CardMedia
-          image="https://images.pexels.com/photos/326514/pexels-photo-326514.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"
-          style={{ height: '330px' }}
-        />
-      </header>
+    <div style={{
+      background:
+      'linear-gradient(to bottom, rgba(0, 0, 0, .5) 0%, ' +
+      'rgba(0, 0, 0, 0.5) 50%, rgba(0, 0, 0, 0) 100%), ' +
+      `url("http://thewaycom.com.br/novosite/wp-content/uploads/2016/12/audiovisual.jpg")` +
+      'no-repeat center center fixed',
+      backgroundSize: 'cover',
+    }}>
       <Paper className={classes.optionsContainer}>
         <Field
           name="search"
@@ -42,7 +43,7 @@ const Main = ({ popularMovies, genres, classes, handleSearch }) => {
         />
       </Paper>
       <MoviesList />
-    </Fragment>
+    </div>
   );
 };
 
