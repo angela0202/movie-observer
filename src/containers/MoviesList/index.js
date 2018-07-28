e48ce6bed900d41bb5281f11412956f979495e74
Angela <ang.hayrapetian@gmail.com>
2018-07-28 18:52:49 +0400

    [add] separate movie container

--- src/containers/MoviesList/index.js
+++ src/containers/MoviesList/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { fetchPopularMovies, fetchMovieGenres } from '../../actions/movieActions';
+import {
+  fetchPopularMovies,
+  fetchMovieGenres,
+} from '../../actions/movieActions';
 
 import MoviesList from '../../components/MoviesList';
 import { addToFavorites } from '../../actions/favorites';
@@ -12,32 +15,37 @@ class MovieListContainer extends Component {
     this.props.fetchMovieGenres();
   }
 
-  onAddToFavorites = (movieID) => {
+  onAddToFavorites = movieID => {
     this.props.addToFavorites(movieID);
   };
 
   render() {
-    return <MoviesList
-      movies={this.props.movies}
-      genres={this.props.genres}
-      genresLoading={this.props.genresLoading}
-      moviesLoading={this.props.moviesLoading}
-      onAddToFavorites={this.onAddToFavorites}
-    />
+    return (
+      <MoviesList
+        movies={this.props.movies}
+        genres={this.props.genres}
+        genresLoading={this.props.genresLoading}
+        moviesLoading={this.props.moviesLoading}
+        onAddToFavorites={this.onAddToFavorites}
+      />
+    );
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   movies: state.movieReducer.movies,
   genres: state.movieReducer.genres,
   moviesLoading: state.movieReducer.moviesLoading,
-  genresLoading: state.movieReducer.genresLoading
+  genresLoading: state.movieReducer.genresLoading,
 });
 
 const mapDispatchToProps = dispatch => ({
   fetchPopularMovies: () => dispatch(fetchPopularMovies()),
   fetchMovieGenres: () => dispatch(fetchMovieGenres()),
-  addToFavorites: (movie) => dispatch(addToFavorites(movie))
+  addToFavorites: movie => dispatch(addToFavorites(movie)),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(MovieListContainer);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MovieListContainer);
